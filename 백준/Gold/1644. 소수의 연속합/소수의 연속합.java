61939d58bca43f6e487ed85e71a033fb38b71e6d
good9804 <51435556+good9804@users.noreply.github.com>
2023-02-09 22:09:45 +0900

    [Gold III] Title: 소수의 연속합, Time: 248 ms, Memory: 34864 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -1,66 +1,74 @@
-import java.lang.*;
+
 import java.io.*;
-import java.lang.reflect.Constructor;
-import java.util.*;
-import java.math.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+import java.util.function.IntPredicate;
+
+
 public class Main {
+	static int logN;
+	static long[] hap;
 
-    public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException{
+	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	int N=Integer.parseInt(br.readLine());
+	if(N==1) {System.out.println(0);return;}
+	boolean[] isPrime=new boolean[N+1];
+	int[] arr=new int[N+1];
+	int index=0;
+	Arrays.fill(isPrime, true);
+	for(int i=2;i<=N;i++) {
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int num = Integer.parseInt(st.nextToken());
-        if(num==1){System.out.println(0);return;}
-        if(num==3){System.out.println(1);return;}
-        if(num==4){System.out.println(0);return;}
-        boolean isPrime[]=new boolean[num+1];
-        for(int i=2;i<=num;i++) {
-            isPrime[i] = true;
-        }
-        int[] arr=new int[num];
-        int index=0;
-        for(int i=2;i<=num;i++){
-
-            if(!isPrime[i]){
-
-                continue;}
-            for(int j=i*2;j<=num;j+=i){
-                isPrime[j]=false;
-
-            }
-        }
-        for(int i=2;i<=num;i++){
-            if(isPrime[i]){arr[index++]=i;}
-        }
-        int count=0;
-        int start=0;
-        int end=0;
-        int sum=0;
-        int end2=0;
-        while(true){
-                if(arr[start]>num/2){if(num!=3&&isPrime[num]){count++;}break;}
-            for(int  i=end2;i<=end;i++){
-                sum+=arr[i];
-            }
-            if(sum>num){
-                start++;
-                end2=start;
-                end=start;
-                sum=0;
-            }else if(sum<num){
-
-                end++;
-                end2=end;
-
-            } else{count++;
-                start++;
-                end2=start;
-                end=start;
-            sum=0;}
-
-        }
-        System.out.println(count);
-    }
-    }
+		if(isPrime[i]) {
+			arr[index++]=i;
+		for(int k=i*2;k<=N;k+=i) {
+			isPrime[k]=false;
+		}
+		}
+		
+		
+	}
+	long sum=0;
+	int answer=0;
+	int start=0;
+	int end=0;
+	int temp=0;
+	while(start<index) {
+		
+		if(arr[start]+arr[start+1]>N) {
+			if(isPrime[N]) {answer++;}break;
+		}
+		for(int i=temp;i<=end;i++) {
+			sum+=arr[i];
+		}
+		if(sum>N) {
+			start++;
+			end=start;
+			temp=start;
+			sum=0;
+		}else if(sum<N) {
+			end++;
+			temp=end;
+		}
+		else {sum=0;
+			answer++;
+			start++;
+			end=start;
+			temp=start;}
+		
+		
+		
+	}
+	System.out.println(answer);
+	
+	
+	}
+	}
