1e4556a5c1f09461f4a7763db30eca95394bded8
good9804 <51435556+good9804@users.noreply.github.com>
2023-02-09 21:30:25 +0900

    [Gold I] Title: 커피숍2, Time: 836 ms, Memory: 76296 KB -BaekjoonHub

--- 백준/Gold/1275. 커피숍2/커피숍2.java
+++ 백준/Gold/1275. 커피숍2/커피숍2.java
@@ -1,85 +1,77 @@
-import java.lang.*;
+
 import java.io.*;
-import java.util.*;
-import java.math.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+
 public class Main {
-static long[] candyindex;
-static int firstindex;
-static int answer;
-    public static void main(String[] args) throws IOException {
+	static int logN;
+	static long[] hap;
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb=new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int size=Integer.parseInt(st.nextToken());
-        int test=Integer.parseInt(st.nextToken());
-        int k=0;
-        double treeindex=0;
-        while(treeindex<size){
-            k++;
-            treeindex=Math.pow(2,k);
-
-        }
-        int allsize=(int)Math.pow(2,k+1);
-        firstindex=(int) treeindex;
-        candyindex=new long[allsize];
-
-        st = new StringTokenizer(br.readLine());
-        int index=firstindex;
-        while (st.hasMoreTokens()){
-            long temp=Long.parseLong(st.nextToken());
-            candyindex[index]=temp;
-            maketree(index++,temp);
-        }
-
-        for(int i=0;i<test;i++){
-            st = new StringTokenizer(br.readLine());
-            int start=Integer.parseInt(st.nextToken());
-            int end=Integer.parseInt(st.nextToken());
-            if(start>end){
-                int a=start;
-                start=end;
-                end=a;
-            }
-            int changeindex=Integer.parseInt(st.nextToken());
-            long newnum=Integer.parseInt(st.nextToken());
-            long sum=0;
-            int left=firstindex+start-1;
-            int right=firstindex+end-1;
-            if(left==right){
-                System.out.println(candyindex[left]);
-            }
-            else {
-                while(left<=right){
-            if(left%2==1){sum+=candyindex[left++]; }
-
-            if(right%2==0){sum+=candyindex[right--];}
-                left/=2;
-                right/=2;
-
-            }
-            System.out.println(sum);}
-
-            maketree(firstindex+changeindex-1,newnum-candyindex[firstindex+changeindex-1]);
-            candyindex[firstindex+changeindex-1]=newnum;
-        }
-
-
-
-    }
-static void maketree(int index,long num){
-        if(index==1){return;}
-        candyindex[index/2]+=num;
-        maketree(index/2,num );
-
+	public static void main(String[] args) throws IOException{
+	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+	StringTokenizer st=new StringTokenizer(br.readLine());
+	int size=Integer.parseInt(st.nextToken());
+	int turn=Integer.parseInt(st.nextToken());
+	logN=0;
+	for(int i=1;i<=size;i*=2){
+		logN++;
+	}
+	
+	hap=new long[(int)Math.pow(2, logN+1)];
+	long firstindex=(long) Math.pow(2, logN);
+	st=new StringTokenizer(br.readLine());
+	for(int i=0;i<size;i++) {
+		makehap((int)firstindex+i, Long.parseLong(st.nextToken()));
+	}
+	StringBuilder sb=new StringBuilder();
+	for(int i=0;i<turn;i++) {
+		st=new StringTokenizer(br.readLine());
+		long sum=0;
+		long start=firstindex+Integer.parseInt(st.nextToken())-1;
+		long end=firstindex+Integer.parseInt(st.nextToken())-1;
+		long changeindex=firstindex+Integer.parseInt(st.nextToken())-1;
+		long num=Long.parseLong(st.nextToken());
+		if(start>end) {
+			long temp=start;
+			start=end;
+			end=temp;
+		}
+		while(end>=start) {
+			if(start%2==1) {
+				sum+=hap[(int)start++];
+			}
+			if(end%2==0) {
+				sum+=hap[(int)end--];
+			}
+		start/=2;
+		end/=2;
+		}
+		sb.append(sum).append("\n");
+		
+		makehap((int)changeindex, num-hap[(int)changeindex]);
+		
+		
+	}
+	
+	System.out.println(sb);
+	
 }
-
-
-
-}
-
-
-
-
-
-
+	
+	static void makehap(int index,long num) {
+		int temp=index;
+		while(temp!=0) {
+			hap[temp]+=num;
+			temp/=2;
+			
+		}
+		
+	}}
