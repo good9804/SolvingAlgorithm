0133d18ca6996355c595791dc943cfb33415c090
이건희 <51435556+good9804@users.noreply.github.com>
2023-05-14 15:44:35 +0900

    [Gold III] Title: 소수의 연속합, Time: 228 ms, Memory: 28604 KB -BaekjoonHub

--- 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
+++ 백준/Gold/1644. 소수의 연속합/소수의 연속합.java
@@ -1,74 +1,65 @@
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
-import java.util.function.IntPredicate;
+import java.awt.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        boolean[] notdigit=new boolean[4000001];
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=2;i<=4000000;i++){
+            if(!notdigit[i]){
+                list.add(i);
+                int temp=2;
+                while(true){
+                    int j=temp*i;
+                    if(j>4000000){break;}
+                    notdigit[j]=true;
+                    temp++;
+                }
+            }
+        }
+        int start=0;
+        int end=1;
+        int num = Integer.parseInt(br.readLine());
+        if(num==1){
+            System.out.println(0);return;
+        }
+        int sum=0;
+        if(!notdigit[num]){sum++;}
+
+        int current=0;
+        current=list.get(start)+list.get(end);
+        while(start<end&&end<list.size()){
+            if(list.get(start)>num){break;}
+            if(current==num){sum++;
+
+                current-=list.get(start);
+                start++;}
+            else if(current>num){
+                current-=list.get(start);
+                start++;
+            }
+            else{
+                end++;
+                current+=list.get(end);
+            }
+        }
+        System.out.println(sum);
+        }
+    }
 
 
-public class Main {
-	static int logN;
-	static long[] hap;
 
-	public static void main(String[] args) throws IOException{
-	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-	int N=Integer.parseInt(br.readLine());
-	if(N==1) {System.out.println(0);return;}
-	boolean[] isPrime=new boolean[N+1];
-	int[] arr=new int[N+1];
-	int index=0;
-	Arrays.fill(isPrime, true);
-	for(int i=2;i<=N;i++) {
 
-		if(isPrime[i]) {
-			arr[index++]=i;
-		for(int k=i*2;k<=N;k+=i) {
-			isPrime[k]=false;
-		}
-		}
-		
-		
-	}
-	long sum=0;
-	int answer=0;
-	int start=0;
-	int end=0;
-	int temp=0;
-	while(start<index) {
-		
-		if(arr[start]+arr[start+1]>N) {
-			if(isPrime[N]) {answer++;}break;
-		}
-		for(int i=temp;i<=end;i++) {
-			sum+=arr[i];
-		}
-		if(sum>N) {
-			start++;
-			end=start;
-			temp=start;
-			sum=0;
-		}else if(sum<N) {
-			end++;
-			temp=end;
-		}
-		else {sum=0;
-			answer++;
-			start++;
-			end=start;
-			temp=start;}
-		
-		
-		
-	}
-	System.out.println(answer);
-	
-	
-	}
-	}
+
+
+
+
