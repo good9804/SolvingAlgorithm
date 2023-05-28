d615fce9a907290dd06c36cab764f89f23f17c96
이건희 <51435556+good9804@users.noreply.github.com>
2023-05-28 16:28:08 +0900

    [Platinum V] Title: 백조의 호수, Time: 1788 ms, Memory: 351804 KB -BaekjoonHub

--- 백준/Platinum/3197. 백조의 호수/백조의 호수.java
+++ 백준/Platinum/3197. 백조의 호수/백조의 호수.java
@@ -12,105 +12,96 @@ import java.util.*;
 class Main {
     static int row;
     static int col;
-    static Queue<Point> water;
-    static Queue<Point> birds;
-    static int[][] pan;
     static int[] dx={-1,1,0,0};
-    static int[] dy={0,0,1,-1};
-    static int[] disjoint;
+    static int[] dy={0,0,0-1,1};
+    static Queue<Point> ice=new LinkedList<>();
+    static int[][] pan;
+    static int[] unionarr;
+    static Point firstDuck;
+    static Point secondDuck;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st=new StringTokenizer(br.readLine());
         row = Integer.parseInt(st.nextToken());
         col = Integer.parseInt(st.nextToken());
-        water = new LinkedList<>();
-        disjoint=new int[row*col];
-        boolean[] Visited=new boolean[row*col];
-        boolean[] Visited2=new boolean[row*col];
-        for(int i=0;i<row*col;i++){
-            disjoint[i]=i;
-        }
         pan=new int[row+1][col+1];
-        birds = new LinkedList<>();
+        unionarr=new int[(row*col)+1];
+        for(int i=1;i<unionarr.length;i++){
+            unionarr[i]=i;
+        }
+        boolean flag=false;
         for(int i=1;i<=row;i++){
             String[] str=br.readLine().split("");
             for(int j=1;j<=col;j++){
-                if(str[j-1].equals(".")){pan[i][j]=0;
-                    water.add(new Point(i,j));
-                }
-                else if(str[j-1].equals("X")){pan[i][j]=1;
-                    }
-                else{pan[i][j]=0;birds.add(new Point(i,j));
-                    water.add(new Point(i,j));
-                    }
+            if(str[j-1].equals("X")){
+                pan[i][j]=1;
+            }else if(str[j-1].equals("L")){
+                pan[i][j]=0;
 
+                ice.add(new Point(i,j));
+                if(!flag){
+                    flag=true;
+                    firstDuck=new Point(i,j);
+                }else secondDuck=new Point(i,j);
+            }else {
+                pan[i][j]=0;
+                ice.add(new Point(i,j));
             }
-        }
-        int bird1;
-        int bird2;
-
-            Point p= birds.poll();
-            bird1=change(p.x,p.y);
-        p= birds.poll();
-        bird2=change(p.x,p.y);
-
-        int count=1;
+        }}
+        int day=0;
         while(true){
-        int wsize=water.size();
-        Arrays.fill(Visited,false);
-        for(int w=0;w<wsize;w++){
-            Point currentwater=water.poll();
-            int currentwaterxy=change(currentwater.x,currentwater.y);
-            for(int i=0;i<4;i++){
-                int tempx=currentwater.x+dx[i];
-                int tempy=currentwater.y+dy[i];
-
-                int tempxy=change(tempx,tempy);
-            if(isRange(tempx,tempy)&&pan[tempx][tempy]==1&&!Visited[tempxy]){
-                Visited[tempxy]=true;
-                water.add(new Point(tempx,tempy));
-                if(find(tempxy)!=find(currentwaterxy)){
-                    union(currentwaterxy,tempxy);
-                }
-                pan[tempx][tempy]=0;
-            }else if(isRange(tempx,tempy)&&pan[tempx][tempy]!=1&&!Visited[tempxy]){
-                Visited[tempxy]=true;
-                if(find(tempxy)!=find(currentwaterxy)){
-                    union(currentwaterxy,tempxy);
-                }
-            }
-
-            }
-        }
-
-            if(find(bird1)==find(bird2)){
-                System.out.println(count-1);
+            day++;
+            melting();
+            if(isMeet()){
+                System.out.println(day-1);
                 return;
             }
-        count++;
-        }
 
-
-        }
-        static boolean isRange(int x,int y){
-        return x>=1&&x<=row&&y>=1&&y<=col;
-        }
-        static void union(int a,int b){
-        a=find(a);
-        b=find(b);
-        disjoint[a]=b;
-        }
-
-        static int find(int a){
-        if(a==disjoint[a]){
-            return a;
-        }else return disjoint[a]=find(disjoint[a]);}
-
-
-        static int change(int x,int y){
-        return (x-1)*col+y-1;
         }
     }
+    static boolean isRange(int x,int y){
+        return x>=1&&x<=row&&y>=1&&y<=col;
+    }
+    static void union(int a,int b){
+        a=find(a);
+        b=find(b);
+        unionarr[a]=b;
+    }
+    static int find(int a){
+        if(unionarr[a]==a){
+            return a;
+        }else return unionarr[a]=find(unionarr[a]);
+    }
+    static boolean isMeet(){
+        if(find((firstDuck.x-1)*col+firstDuck.y)==find((secondDuck.x-1)*col+secondDuck.y)){
+            return  true;
+        }
+        return false;
+    }
+    static void melting(){
+        int icesize = ice.size();
+        boolean[][] isVisted=new boolean[row+1][col+1];
+        for(int s=0;s<icesize;s++){
+            Point currentice = ice.poll();
+            for(int i=0;i<4;i++){
+                int tempx=currentice.x+dx[i];
+                int tempy=currentice.y+dy[i];
+                if(isRange(tempx,tempy)&&pan[tempx][tempy]==1&&!isVisted[tempx][tempy]){
+                    pan[tempx][tempy]=0;
+                    ice.add(new Point(tempx, tempy));
+                    isVisted[tempx][tempy]=true;
+                    if(find((currentice.x-1)*col+currentice.y)!=(tempx-1)*col+tempy){
+                    union((currentice.x-1)*col+currentice.y,(tempx-1)*col+tempy);}
+                }else if(isRange(tempx,tempy)&&pan[tempx][tempy]==0&&!isVisted[tempx][tempy]){
+                    isVisted[tempx][tempy]=true;
+                    if(find((currentice.x-1)*col+currentice.y)!=(tempx-1)*col+tempy){
+                        union((currentice.x-1)*col+currentice.y,(tempx-1)*col+tempy);}
+            }
+        }
+    }
+    }}
+
+
 
 
 
