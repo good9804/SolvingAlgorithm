9bcb8fdde20c63e6f8d1e9db45412dd80b0ac27a
이건희 <51435556+good9804@users.noreply.github.com>
2023-08-09 09:44:17 +0900

    [level 3] Title: 등산코스 정하기, Time: 78.01 ms, Memory: 163 MB -BaekjoonHub

--- 프로그래머스/lv3/118669. 등산코스 정하기/등산코스 정하기.java
+++ 프로그래머스/lv3/118669. 등산코스 정하기/등산코스 정하기.java
@@ -4,83 +4,97 @@ class Solution {
     static  boolean[] cantgo;
     static int intesity=0;
     static boolean[] isChecked;
-    static int[] result;
     static ArrayList<Path>[] arr;
     static HashMap<Integer,Integer> summitsMin=new HashMap<>();
     public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
         N=n;
-        boolean[] isGate=new boolean[N+1];
-        boolean[] isSummit=new boolean[N+1];
+        cantgo=new boolean[N+1];
         isChecked=new boolean[N+1];
         Arrays.sort(summits);
         for(int i=0;i<gates.length;i++){
-            isGate[gates[i]]=true;
+            cantgo[gates[i]]=true;
         }
         for(int i=0;i<summits.length;i++){
-            isSummit[summits[i]]=true;
+            cantgo[summits[i]]=true;
         }
         arr=new ArrayList[n+1];
-        result=new int[n+1];
         for(int i=1;i<=n;i++){
             arr[i]=new ArrayList<Path>();
         }
+        int skip=0;
         for(int i=0;i<paths.length;i++){
             int start=paths[i][0];
             int end=paths[i][1];
             int cost=paths[i][2];
-            if(isGate[start]||isSummit[end]){
-                arr[start].add(new Path(start,end,cost));
-            }else if(isGate[end]||isSummit[start]){
-            arr[end].add(new Path(end,start,cost));
-            }else{
             arr[start].add(new Path(start,end,cost));
-            arr[end].add(new Path(end,start,cost));}
+            arr[end].add(new Path(end,start,cost));
+            
+                   
+               
+            
         }
         
         int[] answer = new int[2];
         int minCost=Integer.MAX_VALUE;
+        boolean flag=false;
+        for(int i=0;i<gates.length;i++){
+            int gate=gates[i];
+            
+                cantgo[gate]=false;
+            for(int j=0;j<summits.length;j++){
+                int summit=summits[j];
+                cantgo[summit]=false;
+                intesity=Integer.MAX_VALUE;
+                isChecked[gate]=true;
+                summitDfs(gate,gate,summit,Integer.MIN_VALUE);
+                /*Arrays.fill(isChecked,false);
+                isChecked[summit]=true;
+                gateDfs(summit,gate,summit,Integer.MIN_VALUE);*/
                 
-               djistra(gates,summits);
-            for(int summit:summits){
-                if(result[summit]<minCost){
+                Arrays.fill(isChecked,false);
+                if(intesity<minCost){
                     answer[0]=summit;
-                    answer[1]=result[summit];
-                    minCost=result[summit];
+                    answer[1]=intesity;
+                    minCost=intesity;
                 }
+                
+                cantgo[summit]=true;
             }
-        
+                cantgo[gate]=true;
+        }
         return answer;
     }
-    static void djistra(int[] gates,int[] summits){
-        Arrays.fill(result,Integer.MAX_VALUE);
-        Queue<Path> q=new LinkedList<>();
-        for(int gate:gates){
-        q.add(new Path(0,gate,0));
-        result[gate]=0;}
-        
-        while(!q.isEmpty()){
-            Path current=q.poll();
-            if(result[current.end]<current.cost){continue;}
-            
-                for(Path p:arr[current.end]){
-                    int c=Math.max(p.cost,result[current.end]);
-                    if(result[p.end]>c){
-                        result[p.end]=c;
-                        q.add(new Path(p.start,p.end,c));
-                    }
-                }
-            
+    static void summitDfs(int current,int gate,int summit,int cost){
+        if(current== summit){
+            intesity=Math.min(intesity,cost);
+            return;
         }
-        
-        
+        for(Path p :arr[current]){
+            if(!isChecked[p.end]&&!cantgo[p.end]){
+                isChecked[p.end]=true;
+                summitDfs(p.end,gate,summit,Math.max(cost,p.cost));
+                isChecked[p.end]=false;
+            }
+        }     
     }
-    
-    
-    
+    static void gateDfs(int current,int gate,int summit,int cost){
+        if(current== gate){
+            
+            intesity=Math.min(intesity,cost);
+            return;
+        }
+        for(Path p :arr[current]){
+            if(!isChecked[p.end]&&!cantgo[p.end]){
+                isChecked[p.end]=true;
+                gateDfs(p.end,gate,summit,Math.max(cost,p.cost));
+                isChecked[p.end]=false;
+            }
+        }     
+    }
     
     
 }
-class Path implements Comparable<Path>{
+class Path {
     int start;
     int end;
     int cost;
@@ -88,10 +102,6 @@ class Path implements Comparable<Path>{
         this.start=start;
         this.end=end;
         this.cost=cost;
-    }
-    @Override
-    public int compareTo(Path p){
-        return this.cost-p.cost;
     }
     
 }
