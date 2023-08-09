2e4a11b98e1bc787485896fcae715ac760602b00
이건희 <51435556+good9804@users.noreply.github.com>
2023-08-09 10:45:41 +0900

    [Silver III] Title: 2×n 타일링, Time: 128 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -1,20 +1,60 @@
-import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-class Main{
-    static int[] answer=new int[1001];
-    public static void main(String[] args) throws IOException{
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        int n=Integer.parseInt(br.readLine());
-        answer[1]=1;
-        answer[2]=2;
-        answer[3]=3;
-        int temp=4;
-        while(temp<=n){
-            answer[temp]=(answer[temp-1]+answer[temp-2])%10007;
-            temp++;
+
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+class Main {
+    static int N;
+    static int T;
+    static int answer=Integer.MAX_VALUE;
+    static int[] dp;
+    static int mod=10007;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //2 * n
+        //1  -> 1
+        //2 -> 2
+        //3 ->3
+        //4 ->5
+        //5->8
+        //6->13
+        //7->21
+        //8->34
+        //9 ->55
+        N = Integer.parseInt(br.readLine());
+        if(N==1){
+            System.out.println(1);
+            return;
         }
-        System.out.println(answer[n]);
+        if(N==2){
+            System.out.println(2);
+            return;
+        }
+        dp=new int[N+1];
+        dp[1]=1;
+        dp[2]=2;
+        for(int i=3;i<=N;i++){
+            dp[i]=(dp[i-1]%mod+dp[i-2]%mod)%mod;
+        }
+        System.out.println(dp[N]);
     }
+
 }
+
+
+
+
+
+
+
+
+
+
