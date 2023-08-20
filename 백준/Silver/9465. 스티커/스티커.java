6de9838bcf1242614cbf8767871716237d9a5613
이건희 <51435556+good9804@users.noreply.github.com>
2023-08-20 23:07:22 +0900

    [Silver I] Title: 스티커, Time: 808 ms, Memory: 111600 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -1,47 +1,47 @@
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.util.*;
 
 
-import java.io.*;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.StringTokenizer;
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb =new StringBuilder();
+        while(T-->0){
+
+            int N = Integer.parseInt(br.readLine());
+            int[][] arr = new int[2][N+1];
+        for (int i = 0; i < 2; i++) {
+            StringTokenizer st=new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                arr[i][j]=Integer.parseInt(st.nextToken());
+            }
+        }
+        int[][] dp1=new int[2][N+1];
+        dp1[0][1]=arr[0][1];
+        dp1[1][1]=arr[1][1];
+        if(N>1){
+            dp1[0][2]=dp1[1][1]+arr[0][2];
+            dp1[1][2]=dp1[0][1]+arr[1][2];
+        }
 
 
+        for(int i=2;i<=N;i++){
+            dp1[0][i]=Math.max(dp1[1][i-1],dp1[1][i-2])+arr[0][i];
+            dp1[1][i]=Math.max(dp1[0][i-1],dp1[0][i-2])+arr[1][i];
 
+        }
 
+        sb.append(Math.max(dp1[0][N],dp1[1][N])+"\n");
+        }
+        System.out.println(sb);
 
-public class Main {
-	
-	public static void main(String[] args) throws IOException {
-			
-	BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-	int test=Integer.parseInt(br.readLine());
-	for(int i=0;i<test;i++) {
-		int size=Integer.parseInt(br.readLine());
-		int[][] arr=new int[2][size+1];
-		int[][]  dp=new int[2][size+1];
-		for(int j=0;j<2;j++) {
-			int index=1;
-			StringTokenizer stringTokenizer=new StringTokenizer(br.readLine());
-			while(stringTokenizer.hasMoreTokens()) {
-				arr[j][index++]=Integer.parseInt(stringTokenizer.nextToken());
-			}
-			
-		}
-		dp[0][1]=arr[0][1];
-		dp[1][1]=arr[1][1];
-		if(size>1) {
-		dp[0][2]=dp[1][1]+arr[0][2];
-		dp[1][2]=dp[0][1]+arr[1][2];}
-		for(int k=1;k<size;k++) {
-			dp[0][k+1]=Math.max(dp[1][k], dp[1][k-1])+arr[0][k+1];
-			dp[1][k+1]=Math.max(dp[0][k], dp[0][k-1])+arr[1][k+1];
-		}
-		System.out.println(Math.max(dp[0][size], dp[1][size]));
-		
-		
-	}
-	
-	
-	}
+    }
 }
+
+
+
