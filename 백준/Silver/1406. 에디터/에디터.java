123251c79589ecaf13e7b0c9b9325d9f31f4e75d
이건희 <51435556+good9804@users.noreply.github.com>
2023-08-23 11:19:39 +0900

    [Silver II] Title: 에디터, Time: 692 ms, Memory: 137744 KB -BaekjoonHub

--- 백준/Silver/1406. 에디터/에디터.java
+++ 백준/Silver/1406. 에디터/에디터.java
@@ -1,50 +1,55 @@
-import java.math.BigInteger;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
-import java.lang.*;
-import java.io.*;
-import java.math.*;
-import java.awt.*;
-import java.util.List;
-
 class Main {
-
+    static int V;
+    static int E;
 
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
-        Stack<Character> leftStack = new Stack<>();
-        Stack<Character> rightStack = new Stack<>();
-        String str= br.readLine();
-        for(int k=0;k<str.length();k++){
-            leftStack.push(str.charAt(k));
+        StringBuilder str = new StringBuilder(br.readLine());
+        int order = Integer.parseInt(br.readLine());
+        Stack<Character> left = new Stack<>();
+        Stack<Character> right = new Stack<>();
+        int loop = str.length();
+        for(int i=0;i<loop;i++){
+            left.push(str.charAt(i));
         }
-        int size = Integer.parseInt(br.readLine());
-        for(int i=0;i<size;i++){
-            String[] arr=br.readLine().split(" ");
-            if(arr[0].equals("P")){
-                leftStack.push(arr[1].charAt(0));
+        while(order-->0){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String ord = st.nextToken();
 
-            }else if(arr[0].equals("L")){
-                if(!leftStack.isEmpty()){
-                    rightStack.push(leftStack.pop());
-                }
-            }else if(arr[0].equals("D")){
+            if(ord.equals("P")){
 
-                if(!rightStack.empty()){
-                    leftStack.push(rightStack.pop());
+                left.push(st.nextToken().charAt(0));
+            }//왼쪽에추가
+            else if(ord.equals("L")){
+                if(!left.isEmpty()){
+                    right.push(left.pop());
                 }
-            }else{
-                if(!leftStack.empty()){
-                    leftStack.pop();
+            }//커서왼쪽
+            else if(ord.equals("D")){
+                if(!right.isEmpty()){
+                    left.push(right.pop());
                 }
+            }//커서오른쪽
+            else{
+                if(!left.isEmpty()){
+                    left.pop();
+                }
+            }//왼쪽문자삭제
         }
-}while(!leftStack.isEmpty()){
-                rightStack.push(leftStack.pop());
-            }
-            while(!rightStack.isEmpty()){
-                sb.append(rightStack.pop());
-            }
-        System.out.println(sb);
-    }}
+        str.setLength(0);
+        while (!left.isEmpty()){
+            str.append(left.pop());
+        }
+        str=str.reverse();
+        while (!right.isEmpty()){
+            str.append(right.pop());
+        }
+        System.out.println(str);
+
+        }
+    }
