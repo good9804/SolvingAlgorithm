25460e7c38326badfbd88479439d9af58a3347db
이건희 <51435556+good9804@users.noreply.github.com>
2023-08-28 16:34:57 +0900

    [Gold V] Title: 동전 1, Time: 132 ms, Memory: 14120 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.java
+++ 백준/Gold/2293. 동전 1/동전 1.java
@@ -1,44 +1,32 @@
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.*;
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        long[] dp=new long[K+1];
+        int[]  arr=new int[N];
+        for(int i=0;i<arr.length;i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }// 1 2 3 4 5 6 7 8 9 10
+        // dp[1],
+        for(int i=0;i<arr.length;i++){
+            //동전봅기
+            for(int j=1;j<=K;j++){
+                if(arr[i]==j){dp[j]++;}
+                if(j-arr[i]>0){//2원짜리로 6원갈때 dp[2]+=1 dp[4]= dp[2] +1 dp[6]= dp[2] 2 dp[4] +1 [dp[6]
+                dp[j]=dp[j-arr[i]]+dp[j];
+            }
+        }}
+        System.out.println(dp[K]);
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
 
 
 
-
-
-
-
-
-public class Main {
-	public static void main(String[] args) throws IOException{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st=new StringTokenizer(br.readLine());
-		int size=Integer.parseInt(st.nextToken());
-		int end=Integer.parseInt(st.nextToken());
-		int[] arr=new int[size+1];
-		int[] dp=new int[end+1];
-		for(int i=1;i<=size;i++) {
-			arr[i]=Integer.parseInt(br.readLine());
-		}
-		
-		for(int i=1;i<=size;i++) {
-			
-			for(int j=1;j<=end;j++) {
-				if(arr[i]==j) {dp[j]++;}
-				if(arr[i]>j) {continue;}
-				dp[j]=dp[j-arr[i]]+dp[j];
-			}
-		}
-		System.out.println(dp[end]);
-	}}
+}}
