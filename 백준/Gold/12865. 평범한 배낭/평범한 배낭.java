7e333a83b4f520bef259fc9585314b5d51b12edc
이건희 <51435556+good9804@users.noreply.github.com>
2023-08-30 16:59:53 +0900

    [Gold V] Title: 평범한 배낭, Time: 188 ms, Memory: 53724 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -4,52 +4,44 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Array;
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.awt.*;
 import java.util.*;
 
 class Main {
     static int N;
     static int K;
-    static int[][] items;
-    static int[][] dp;
-    static int answer=Integer.MIN_VALUE;
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
-        items=new int[N][K+1];
-        dp=new int[N+1][K+1];
-        for(int i=0;i<N;i++){
+        int[][] dp=new int[N+1][K+1];
+        int[][] arr=new int[2][N+1];
+        for(int i=1;i<=N;i++){
             st = new StringTokenizer(br.readLine());
-            items[i][0]=Integer.parseInt(st.nextToken());
-            items[i][1]=Integer.parseInt(st.nextToken());
+            int W = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+            arr[0][i]=W;
+            arr[1][i]=V;
         }
-       for(int i=1;i<=N;i++){
-           findAnswer(i);
-       }
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=K;j++){
+                if(j-arr[0][i]>=0){
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i-1][j-arr[0][i]]+arr[1][i]);
+
+                }else dp[i][j]=dp[i-1][j];
+
+            }
+        }
+
+
         System.out.println(dp[N][K]);
 
-    }
 
-    static void findAnswer(int n){
-        for(int k=1;k<=K;k++){
-        if(k-items[n-1][0]>=0){
-        dp[n][k]=Math.max(dp[n-1][k],dp[n-1][k-items[n-1][0]]+items[n-1][1]);}
-        else dp[n][k]=dp[n-1][k];}
-    }
 
 
     }
 
 
-
-
-
-
-
-
+}
 
 
