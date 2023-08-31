11160672f52a425d3b373f30b2c1cce438ec80fa
이건희 <51435556+good9804@users.noreply.github.com>
2023-08-31 09:46:58 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 792 ms, Memory: 69192 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -1,100 +1,115 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
+import java.math.BigInteger;
+import java.util.*;
+
+class Main {
+    static int N;
+    static int E;
+    static  ArrayList<Node>[] list;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+       //무방향 그래프 1->N번 정점으로 최단거리이동
+         list=new ArrayList[N+1];
+        int[] costs=new int[N+1];
+        boolean[] visited=new boolean[N+1];
+        for(int i=1;i<=N;i++){
+            list[i] = new ArrayList<>();
+            costs[i]=9876543;
+        }
+        for(int i=0;i<E;i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            list[a].add(new Node(b,cost));
+            list[b].add(new Node(a,cost));
+        }
+        st = new StringTokenizer(br.readLine());
+        int v1 = Integer.parseInt(st.nextToken());
+        int v2 = Integer.parseInt(st.nextToken());
+
+        //1  v1 v2     N
+        //1  v2 v1   N
+        PriorityQueue<Node> pq=new PriorityQueue<>();
+        costs[1]=0;
+        //1~ N구한다음에 분할 해서 구하기?
+        pq.add(new Node(1,0));
+        while(!pq.isEmpty()){
+            Node cur=pq.poll();
+            if(cur.cost>costs[cur.end]){
+                continue;
+            }
+
+            for(Node next: list[cur.end]){
+                if(costs[cur.end]+next.cost<costs[next.end]){
+                    costs[next.end]=costs[cur.end]+next.cost;
+                    pq.add(new Node(next.end,costs[next.end]));
+                }
+
+            }
+        }
+        if(costs[v1]==9876543||costs[v2]==9876543||costs[N]==9876543){
+            System.out.println(-1);
+            return;
+        }
+        
+            System.out.println(Math.min(costs[v1]+find(v1,v2)+find(v2,N),costs[v2]+find(v2,v1)+find(v1,N)));
+        
 
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Stack;
-import java.util.StringTokenizer;
 
 
 
 
 
 
+    }
+    static int find(int start,int end){
+        int[] costs=new int[N+1];
+        Arrays.fill(costs,98765432);
+        PriorityQueue<Node> pq=new PriorityQueue<>();
+        costs[start]=0;
+        //1~ N구한다음에 분할 해서 구하기?
+        pq.add(new Node(start,0));
+        while(!pq.isEmpty()){
+            Node cur=pq.poll();
+            if(cur.cost>costs[cur.end]){
+                continue;
+            }
 
-public class Main {
-	static int[] arr;
-	static int size;
-	static int logn;
-	static long[] node;
-	static int first;
-	static int result;
-	static ArrayList<Node>[] lists;
-	
-	public static void main(String[] args) throws IOException{
-		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st=new StringTokenizer(br.readLine());
-		size=Integer.parseInt(st.nextToken());
-		int line=Integer.parseInt(st.nextToken());
-		
-		lists=new ArrayList[size+1];
-		for(int i=1;i<=size;i++) {
-			
-			lists[i]= new ArrayList<Node>();
-		}
-		for(int i=0;i<line;i++) {
-			
-			st=new StringTokenizer(br.readLine());
-			int first=Integer.parseInt(st.nextToken());
-			int end=Integer.parseInt(st.nextToken());
-			int cost=Integer.parseInt(st.nextToken());
-			lists[first].add(new Node(end, cost));
-			lists[end].add(new Node(first, cost));
-		}
-		
-		st=new StringTokenizer(br.readLine());
-		int node1=Integer.parseInt(st.nextToken());
-		int node2=Integer.parseInt(st.nextToken());
-		int temp1=findpath(1,node1 )+findpath(node1, node2)+findpath(node2, size);
-		int temp2=findpath(1,node2 )+findpath(node2, node1)+findpath(node1, size);
-		if(temp1>= 200000000 || temp2>=200000000) {
-			System.out.println(-1);
-		}else System.out.print(Math.min(temp1, temp2));
-		
-	
-	
-	}
+            for(Node next: list[cur.end]){
+                if(costs[cur.end]+next.cost<costs[next.end]){
+                    costs[next.end]=costs[cur.end]+next.cost;
+                    pq.add(new Node(next.end,costs[next.end]));
+                }
 
-	static int findpath(int start,int end) {
-			
-		PriorityQueue<Node> pq=new PriorityQueue<Node>();
-		int[] result=new int[size+1];
-		Arrays.fill(result, 200000000);
-			result[start]=0;
-			pq.add(new Node(start, 0));
-			while(!pq.isEmpty()) {
-				Node current=pq.poll();
-				for(Node next:lists[current.end]) {
-					if(result[next.end]>result[current.end]+next.cost) {
-						result[next.end]=result[current.end]+next.cost;
-						pq.add(new Node(next.end, result[next.end]));
-					}
-				}
-				
-			}
-			return result[end];
-	
-	}
+            }
+        }
+        return costs[end];
+    }
+
 
 }
 class Node implements Comparable<Node>{
-	int end;
-	int cost;
-	public Node(int end,int cost) {
-		this.end=end;
-		this.cost=cost;
-	}
-	@Override
-public int compareTo(Node o) {
-	// TODO Auto-generated method stub
-	return Integer.compare(this.end, o.cost);
+    int end;
+    int cost;
+
+    public Node(int end,int cost){
+        this.end=end;
+        this.cost=cost;
+    }
+    @Override
+    public int compareTo(Node n){
+        return this.cost-n.cost;
+    }
+
+
 }
-}
+
+
