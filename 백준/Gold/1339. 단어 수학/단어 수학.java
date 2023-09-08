ad73d948c54f43e55603d46faf6ca610e29739a6
이건희 <51435556+good9804@users.noreply.github.com>
2023-09-08 15:16:17 +0900

    [Gold IV] Title: 단어 수학, Time: 128 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Gold/1339. 단어 수학/단어 수학.java
+++ 백준/Gold/1339. 단어 수학/단어 수학.java
@@ -1,57 +1,73 @@
 
+import java.awt.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.math.BigInteger;
 import java.util.*;
-import java.lang.*;
-import java.io.*;
-import java.math.*;
-import java.awt.*;
-import java.util.List;
 
 class Main {
+    static int N;
+    static int count;
+    static int[] dx={1,-1,-1,1};
+    static int[] dy={1,-1,1,-1};
+    static int[] arr;
+    static boolean[] isChecked;
+    static boolean[] isfinished;
     public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<String> str = new ArrayList<>();
-        int size = Integer.parseInt(br.readLine());
-        int[] alpha = new int[26];
-        boolean[] alpha2 = new boolean[26];
-        int index=0;
-        int[] lengtharr = new int[size];
-        int c=0;
-        int count=9;
-        int answer=0;
-        boolean bol=false;
-        for(int i=0;i<size;i++){
-            String[] s=br.readLine().split("");
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        HashMap<Character,Integer> map=new HashMap<>();
+        ArrayList<String> list = new ArrayList<>();
+        for(int i=0;i<N;i++){
+            String str = br.readLine();
+            list.add(str);
             int temp=1;
-            for(int k=s.length-1;k>=0;k--){
-                int a=s[k].charAt(0);
-                alpha[a-65]+=temp;
+            for(int j=str.length()-1;j>=0;j--){
+                char c=str.charAt(j);
+                map.put(c,map.getOrDefault(c,0)+temp);
                 temp*=10;
             }
         }
-        for(int i=0;i<26;i++){
-            bol=false;
-            int num1=alpha[i];
-            c=num1;
-            for(int k=0;k<26;k++){
-                int num2 = alpha[k];
-                if(c<num2&&!alpha2[k]){
-                    c=num2;
-                    index=k;
-                    bol=true;
-
-                }
-
+        PriorityQueue<Node> pq=new PriorityQueue<>();
+        for(Character c:map.keySet()){
+            pq.add(new Node(c,map.get(c)));
+        }
+        int count=9;
+        int[] value=new int[26];
+        while(!pq.isEmpty()){
+            value[pq.poll().c-'A']=count--;
+        }
+        int answer=0;
+        for(String str:list){
+            StringBuilder sb = new StringBuilder();
+            for(int i=0;i<str.length();i++){
+                sb.append(value[str.charAt(i)-'A']);
             }
-            if(bol){
-            alpha2[index]=true;
-            answer+=c*count;
-            count--;}
+            answer+=Integer.parseInt(sb.toString());
         }
         System.out.println(answer);
 
 
+    }
 
-}
-}
+
+    }
+    class Node implements Comparable<Node>{
+    char c;
+    int num;
+    public Node(char c,int num){
+        this.c=c;
+        this.num=num;
+    }
+    @Override
+        public int compareTo(Node n){
+        return n.num-this.num;
+    }
+    }
+
+
+
+
