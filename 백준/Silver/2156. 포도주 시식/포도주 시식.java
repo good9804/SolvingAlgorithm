b433d73b17739979d8e20051f7da8a4f038894f8
good9804 <51435556+good9804@users.noreply.github.com>
2023-10-22 01:10:53 +0900

    [Silver I] Title: 포도주 시식, Time: 152 ms, Memory: 15052 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -4,48 +4,45 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Array;
+import java.math.BigInteger;
 import java.util.*;
-
+import java.util.List;
 
 class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
+        //연속으로 놓여있는 3잔못마심
+        //가장 많은양의 포도주
+        //1 2 3 4 5
+        // 4로 올수잇는 경우의수 1 2 4,34 1 3 4
+
         int[] arr=new int[N+1];
         long[] dp=new long[N+1];
         for(int i=1;i<=N;i++){
-            arr[i]=Integer.parseInt(br.readLine());
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        if(N==1){
-            System.out.println(arr[1]);
-            return;
-        }
-        if(N==2){
-            System.out.println(arr[1]+arr[2]);
-            return;
-        }
-        
+        long answer=0;
         dp[1]=arr[1];
+        answer=arr[1];
+        if(N>=2){
         dp[2]=arr[1]+arr[2];
-        // 6 10 13 9 8 1
-        // 6 16 16 25 33 33
-        long answer=dp[2];
+        answer=(int)dp[2];}
+
         for(int i=3;i<=N;i++){
-            dp[i]=Math.max(dp[i-3]+arr[i-1]+arr[i],Math.max(dp[i-2]+arr[i],dp[i-1]));
+            dp[i]=Math.max(Math.max(dp[i-2]+arr[i],dp[i-1]),dp[i-3]+arr[i-1]+arr[i]);
             answer=Math.max(answer,dp[i]);
-            //100 100 1 1 100 100
-            //1     2  3 4 5   6
-            //2-3-5
-            //1-2-5
-            //1-2-4-5
+
+
         }
-        //굳이!!!!! 맨마지막으로가는게목표가아님!!
-
-
         System.out.println(answer);
+
+
 
     }
 }
+
 
 
 
