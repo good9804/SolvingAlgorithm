71ed9c513877b12960e98400dc1a1eced30231d9
good9804 <51435556+good9804@users.noreply.github.com>
2023-10-22 00:51:10 +0900

    [Gold IV] Title: 줄세우기, Time: 120 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Gold/2631. 줄세우기/줄세우기.java
+++ 백준/Gold/2631. 줄세우기/줄세우기.java
@@ -6,36 +6,35 @@ import java.io.InputStreamReader;
 import java.lang.reflect.Array;
 import java.math.BigInteger;
 import java.util.*;
+import java.util.List;
 
 class Main {
-    static int N;
+
     public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());//전체사이즈-가장 긴 증가 수열 길이?
-        int[] arr=new int[N];
-        int[] dp=new int[N];
-        for(int i=0;i<N;i++){
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr=new int[N+1];
+        for(int i=1;i<=N;i++){
             arr[i] = Integer.parseInt(br.readLine());
         }
-        int maxLength=0;
-        for(int i=0;i<N;i++){
-            dp[i]=1;
-           for(int j=0;j<i;j++){
-               if(arr[j]<arr[i]&&dp[j]>=dp[i]){
-                   dp[i]=dp[j]+1;
-                   maxLength=Math.max(dp[i],maxLength);
-               }
-           }
+        int[] lisDp=new int[N+1];
+        int answer=Integer.MIN_VALUE;
+        for(int i=1;i<=N;i++){
+            lisDp[i]=1;
+            for(int j=1;j<i;j++){
+                if(arr[i]>arr[j]&&lisDp[j]>=lisDp[i]){
+                    lisDp[i]=lisDp[j]+1;
+                }
+            }
+            answer=Math.max(answer,lisDp[i]);
         }
-        System.out.println(N-maxLength);
+        System.out.println(N-answer);
 
 
-        }
 
 
     }
-
-
+}
 
 
 
